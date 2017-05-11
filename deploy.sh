92ea0dc9373e41e0441999cd46f936a21b848970
Bekaryss <bekarys.kuralbai@gmail.com>
2017-05-11 10:06:01 +0600

    bash sh

--- deploy.sh
+++ deploy.sh
@@ -110,23 +110,22 @@ fi
 selectNodeVersion
 
 # 3. Install NPM packages
-if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
+if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then  
   cd "$DEPLOYMENT_TARGET"
   eval $NPM_CMD install --production
+  eval $NPM_CMD install --only=dev
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
-fi
 
 
-# 4. Install Bower modules
-if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
+
+# 4. Angular Prod Build
+if [ -e "$DEPLOYMENT_TARGET/.angular-cli.json" ]; then  
   cd "$DEPLOYMENT_TARGET"
-  eval rm -rf bower_components
-  echo deleted bower components
-  eval ./node_modules/.bin/bower install
-  exitWithMessageOnError "bower failed"
+  eval ./node_modules/.bin/ng build --prod
+  exitWithMessageOnError "Angular build failed"
   cd - > /dev/null
-fi
+fi  
 
 # 5. Install Composer modules
 if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
